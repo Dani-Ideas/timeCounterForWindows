6d9486d8bdd7c86170b20c8ba450930ddb6b93ff
Dani-Ideas <d7n456.17@gmail.com>
2022-09-10 11:57:00 -0500

    comiint add all

---
+++ parking_V0.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+enum typeCar {car,truck,sport,limousine,bus,van,tractor,moto};
+
+int parkTimeMinutsMinimun= 30;
+unsigned char dailyWorkHours;
+// testVariablesDeclarations
+extern int a, b, c;
+float f, g, h;
+
+int main()
+{
+//Varible definition
+
+int a, b, c;
+
+//Variable Inizaliztion
+    a = 1;
+    b = 12;
+
+    c = a + b;
+    printf("Value of the sum %d", c);
+    return 0; 
+}
--- timeCounter.c
+++ timeCounter.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int hours, minuts, seconds, timeLapsOnMiliseconds;
-
-//int n = 20;
+int n = 20, carroDentro;
 int main()
 {
-    timeLapsOnMiliseconds = 1000; //equal a 1 second
-
-    for (hours = 0; hours < 24; hours++)
+    carroDentro = 0 ;
+    printf("preciona el 1 para ingresar al estacionamiento");
+    scanf("%d", &carroDentro);
+    if (carroDentro ==1)
     {
-        for (minuts = 0; minuts < 60; minuts++)
-        {
-            for (seconds = 0; seconds < 60; seconds++)
-            {
-                printf("%i:%i:%i\n",hours,minuts,seconds);
-                usleep(timeLapsOnMiliseconds);
-            }
-            
-        }
-        
-    } 
-    //system ("pause");
-    return 0;
-}
-/*
-    while (n>10)
+        while (n>10)
     {
         printf("n es igual a %d\n", n);
         n=n-1;
     }
     printf("ya se salio del bucle porque n es igual a 10");
+    }
+    else
+    {
+        printf("Bueno supongo que no deceas entar al estacionamiento hasta pronto👋\n");
+    }
+    return 0;
+}
+/*
+
 */
